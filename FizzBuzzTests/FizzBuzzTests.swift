8a5b5f06ad504b7df3e893253ec83eeefa94d0e0
Hikmat <asephikmatf@gmail.com>
2020-01-02 00:31:10 +0700

    we start now!

--- FizzBuzzTests/FizzBuzzTests.swift
+++ FizzBuzzTests/FizzBuzzTests.swift
@@ -22,7 +22,6 @@ class FizzBuzzTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertEqual("1", "1")
     }
 
     func testPerformanceExample() {
