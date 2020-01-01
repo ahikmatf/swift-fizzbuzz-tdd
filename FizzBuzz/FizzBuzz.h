f431c80bcb202765f00de3cb83b516468221703b
Hikmat <asephikmatf@gmail.com>
2020-01-02 00:24:52 +0700

    Initial Commit

---
+++ FizzBuzz/FizzBuzz.h
@@ -0,0 +1,19 @@
+//
+//  FizzBuzz.h
+//  FizzBuzz
+//
+//  Created by Fatah on 02/01/20.
+//  Copyright © 2020 Fatah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FizzBuzz.
+FOUNDATION_EXPORT double FizzBuzzVersionNumber;
+
+//! Project version string for FizzBuzz.
+FOUNDATION_EXPORT const unsigned char FizzBuzzVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FizzBuzz/PublicHeader.h>
+
+
